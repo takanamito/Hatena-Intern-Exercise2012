083e8a5d1ed525c70bbd137488d230c9584b06a5
takanamito <takanamito0928@gmail.com>
2012-08-08 01:44:18 +0900

    Template関数とその他変数名を変更

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -1,9 +1,6 @@
 var Template = function(input) {
     // この関数を実装してください
-    var html = input;
-    html_str = html.source;
-    
-    return this;
+    this.html = input.source;
 };
 
 Template.prototype = {
@@ -11,27 +8,28 @@ Template.prototype = {
         // この関数を実装してください
         var title_str = variables.title;
         var content_str = variables.content;
-        var value = new Array(title_str, content_str);
+        var str = new Array(title_str, content_str);
         
         // htmlエスケープ
-        for (var val in value)
-        {
-	        value[val] = escape(value[val]);
+        for (var val in str) {
+	        str[val] = escape(str[val]);
         }
         
         // 置換
-        var output_html = html_str.replace("{% title %}", value[0]);
-        output_html = output_html.replace("{% content %}", value[1]);
+        this.html = this.html.replace(/{%\s*title\s*%}/, str[0]);
+        this.html = this.html.replace(/{%\s*content\s*%}/, str[1]);
         
-        return output_html;
+        return this.html;
     }
 };
 
 // htmlエスケープ用関数
 function escape(string) {
 	string = string.replace(/&/g, "&amp;");
-	string = string.replace(/</g,"&lt;");    
+	string = string.replace(/</g,"&lt;");   
     string = string.replace(/>/g,"&gt;");
+    string = string.replace(/"/g, "&quot;");
+    string = string.replace(/'/g, "&#039;");
     
     return string;
 }
