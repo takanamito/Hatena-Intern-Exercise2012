aef3e2465141099982fb987adfd3acc678f0c873
takanamito <takanamito0928@gmail.com>
2012-08-07 21:23:15 +0900

    課題1-1 修正

--- exercise1-1/lib/TemplateEngine.pm
+++ exercise1-1/lib/TemplateEngine.pm
@@ -7,6 +7,7 @@ package TemplateEngine;
 
 # html用の配列
 our @html = {};
+our @escape_arr = {};
 
 # htmlファイルパスから中身を呼び出す
 sub new {
@@ -26,21 +27,21 @@ sub new {
 # html書き換えて出力
 sub render {
 	my ($name, $hash) = @_;
-	
 	my %hash = %$hash;	# ハッシュをデリファレンス
 		
 	my $title = $hash{'title'};
 	my $content = $hash{'content'};
-		
+	@escape_arr = {$title, $content};
+	
+	# エスケープ
+	my @escaped = &escape (@escape_arr);
+	
 	# ループさせてhtml書き換え
-	foreach my $line (@html) 
-	{
-		if ($line =~/title/) 
-		{
+	foreach my $line (@html) {
+		if ($line =~/title/) {
 			$line =~s/{% title %}/$title/;
 		}
-		elsif ($line =~/content/) 
-		{
+		elsif ($line =~/content/) {
 			$line =~s/{% content %}/$content/;
 		}
 	}
@@ -49,6 +50,22 @@ sub render {
 	open (NEWFILE, ">:encoding(utf8)", "output.html");
 	print NEWFILE @html;
 	close (NEWFILE);
+
+	return @html;
+}
+
+
+# エスケープ用サブルーチン
+sub escape {
+	foreach my $str (@escape_arr) {
+		$str =~ s/&/&amp;/g;
+		$str =~ s/</&lt;/g;
+		$str =~ s/>/&gt;/g;
+		$str =~ s/\"/&quot;/g;
+		$str =~ s/\'/&#39;/g; 
+		
+		return($str);
+	}
 }
 
 1;
