aee35877fa83d1e14f5380fd73fb89700ac232ba
takanamito <takanamito0928@gmail.com>
2012-08-08 11:58:20 +0900

    課題2-2 実装
    URLが含まれていた場合リンクを貼る処理を実装

---
+++ exercise2-2/template.js
@@ -0,0 +1,43 @@
+var Template = function(input) {
+    // この関数を実装してください
+    this.html = input.source;
+};
+
+Template.prototype = {
+    render: function(variables) {
+        // この関数を実装してください
+        var title_str = variables.title;
+        var content_str = variables.content;
+        var str = new Array(title_str, content_str);
+        
+        // htmlエスケープ
+        for (var val in str) {
+	        str[val] = escape(str[val]);
+        }
+        
+        // リンク処理
+        var search_url = str[1].indexOf("http");
+        
+        if(search_url != -1 ) {
+	        str[1] = str[1].replace(/(http:\/\/[\x21-\x7e]+)/gi, "\<a href='$1'>$1<\/a>");
+	        console.log(str[1]);
+        }
+        
+        // 置換
+        this.html = this.html.replace(/{%\s*title\s*%}/, str[0]);
+        this.html = this.html.replace(/{%\s*content\s*%}/, str[1]);
+        console.log(this.html);
+        return this.html;
+    }
+};
+
+// htmlエスケープ用関数
+function escape(string) {
+	string = string.replace(/&/g, "&amp;");
+	string = string.replace(/</g,"&lt;");   
+    string = string.replace(/>/g,"&gt;");
+    string = string.replace(/"/g, "&quot;");
+    string = string.replace(/'/g, "&#039;");
+    
+    return string;
+}
